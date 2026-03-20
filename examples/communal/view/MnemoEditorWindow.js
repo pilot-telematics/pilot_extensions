cb80b91ee6b7ccc4e5f62bacc4013af2b1d16904
boooch <s@pilot-gps.com>
2026-03-20 16:42:42 +0300

    buttons

--- examples/communal/view/MnemoEditorWindow.js
+++ examples/communal/view/MnemoEditorWindow.js
@@ -275,7 +275,7 @@ Ext.define('Store.communal.view.MnemoEditorWindow', {
                     }, {
                         xtype: 'button',
                         tooltip: l('Delete selected'),
-                        iconCls: "fa fa-thrash-alt",
+                        iconCls: "fa fa-trash-can",
                         handler: function () {
                             this.up('window').removeSelectedElement();
                         }
